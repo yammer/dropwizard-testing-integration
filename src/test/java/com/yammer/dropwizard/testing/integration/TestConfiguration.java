b02df2997ff992948b00971807c6e2cf8bf5bdaf
Michał Rutkowski <mrutkowski@yammer-inc.com>
2013-09-30 11:35:20 +0100

    library extraction complete, updated pom

--- src/test/java/com/yammer/dropwizard/testing/integration/TestConfiguration.java
+++ src/test/java/com/yammer/dropwizard/testing/integration/TestConfiguration.java
@@ -2,11 +2,7 @@ package com.yammer.dropwizard.testing.integration;
 
 import com.yammer.dropwizard.config.Configuration;
 
-/**
- * User: mrutkowski
- * Date: 9/30/13
- * Time: 9:02 AM
- */
+
 public class TestConfiguration extends Configuration {
     private String filename;
 
